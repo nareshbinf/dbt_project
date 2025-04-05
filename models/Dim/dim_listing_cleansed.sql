3e846a923ad1b9be8aa16f86506de9712bd4b93a
Naresh Batchu <nareshb.inf@gmail.com>
2025-04-05 14:31:57 +0000

    dbt

---
+++ models/Dim/dim_listing_cleansed.sql
@@ -0,0 +1,14 @@
+with src_listings as (
+    select * from {{ref(src_listings)}}
+    )
+select 
+ID, 
+LISTING_URL, 
+NAME, 
+ROOM_TYPE, 
+case when MINIMUM_NIGHTS =0 then 1 else MINIMUM_NIGHTS end as MINIMUM_NIGHTS,
+HOST_ID, 
+replace(PRICE,'$') :: number(10,2) as PRICE, 
+CREATED_AT, 
+UPDATED_AT
+from src_listings
