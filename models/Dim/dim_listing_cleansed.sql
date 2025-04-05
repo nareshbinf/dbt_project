305a0dfd98dac2538b0f665e9a669a83f53a0424
Naresh Batchu <nareshb.inf@gmail.com>
2025-04-05 14:36:18 +0000

    dbt

--- models/Dim/dim_listing_cleansed.sql
+++ models/Dim/dim_listing_cleansed.sql
@@ -8,7 +8,7 @@ NAME,
 ROOM_TYPE, 
 case when MINIMUM_NIGHTS =0 then 1 else MINIMUM_NIGHTS end as MINIMUM_NIGHTS,
 HOST_ID, 
-replace(PRICE,'$') :: number(10,2) as PRICE, 
+cast(replace(PRICE,'$', ) number(10,2)) as PRICE_num, 
 CREATED_AT, 
 UPDATED_AT
 from src_listings
