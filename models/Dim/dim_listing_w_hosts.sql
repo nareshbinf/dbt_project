a1db5869f7c095a3a3cf83926ac9d5862caebace
Naresh Batchu <nareshb.inf@gmail.com>
2025-04-06 19:53:19 +0000

    dbt

---
+++ models/Dim/dim_listing_w_hosts.sql
@@ -0,0 +1,15 @@
+with l as (
+    select * from {{ref('dim_listing_cleansed')}}
+)
+
+l as (
+    select * from {{ref('DIM_HOST_CLEANSED')}}
+)
+
+select 
+l.ID as listing_id, l.NAME as listing_name, l.ROOM_TYPE, l.MINIMUM_NIGHTS, l.PRICE,
+l.HOST_ID, h.NAME as HOST_NAME, 
+h.IS_SUPERHOST AS host_is_superhost, l.CREATED_AT, 
+greatest(l.UPDATED_AT, h.UPDATED_AT)
+from l
+left join h on (h.HOST_ID = l.HOST_ID)
