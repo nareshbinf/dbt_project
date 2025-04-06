514eebba2542989d70354f834ac6a60f6353b822
Naresh Batchu <nareshb.inf@gmail.com>
2025-04-06 19:56:31 +0000

    dbt

--- models/Dim/dim_listing_w_hosts.sql
+++ models/Dim/dim_listing_w_hosts.sql
@@ -10,6 +10,6 @@ select
 l.ID as listing_id, l.NAME as listing_name, l.ROOM_TYPE, l.MINIMUM_NIGHTS, l.PRICE,
 l.HOST_ID, h.NAME as HOST_NAME, 
 h.IS_SUPERHOST AS host_is_superhost, l.CREATED_AT, 
-greatest(l.UPDATED_AT, h.UPDATED_AT)
+GREATEST(l.UPDATED_AT, h.UPDATED_AT)
 from l
 left join h on (h.HOST_ID = l.HOST_ID)
