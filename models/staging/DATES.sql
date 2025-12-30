9219ceabde8f142e61f211d4d9f1dabde4e7b1ec
Naresh Batchu <nareshb.inf@gmail.com>
2025-12-30 04:11:58 +0000

    dbt

--- models/staging/DATES.sql
+++ models/staging/DATES.sql
@@ -2,7 +2,7 @@
 {{config(materialized='incremental', unique_key = 'd_date')}}
 
 select d.*, current_timestamp as create_date from SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL.DATE_DIM d
-WHERE D_DATE < CURRENT_DATE
+WHERE D_DATE < CURRENT_DATE+1
 
 {%if is_incremental() %}
  and d_date > (select max(d_date) from {{this}})
