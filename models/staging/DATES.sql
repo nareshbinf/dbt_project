edce368ff01ec71f2fe4bfe2c6bb6e4289468e45
Naresh Batchu <nareshb.inf@gmail.com>
2025-12-30 03:52:00 +0000

    dbt

--- models/staging/DATES.sql
+++ models/staging/DATES.sql
@@ -1,5 +1,5 @@
 
-{{config(materialized='incremetal', unique_key = 'd_date')}}
+{{config(materialized='incremental', unique_key = 'd_date')}}
 
 select * from SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL.DATE_DIM
 WHERE D_DATE < CURRENT_DATE
