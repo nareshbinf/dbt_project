684a20a80054cb6aa72b8141a3011306105dd30f
Naresh Batchu <nareshb.inf@gmail.com>
2025-12-30 03:50:15 +0000

    dbt

---
+++ models/staging/DATES.sql
@@ -0,0 +1,9 @@
+
+{{config(materialized='incremetal', unique_key = 'd_date')}}
+
+select * from SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL.DATE_DIM
+WHERE D_DATE < CURRENT_DATE
+
+{%if is_incremental() %}
+ and d_date > (select max(d_date) from {{this}})
+ {% endif %}
